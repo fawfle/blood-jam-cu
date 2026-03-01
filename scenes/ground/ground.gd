3a39c452846a4da08265872af766d642d2cd9470
fawfle <0neofthoseguys123@gmail.com>
2026-02-28 18:25:29 -0700

    fixed resizing, i'm a little stupid

--- scenes/ground/ground.gd
+++ scenes/ground/ground.gd
@@ -5,15 +5,20 @@ var blood_image: Image;
 
 @export var blood_color: Color = Color(1, 0, 0, 1)
 
-var room_size: Vector2i = Vector2i(288, 162)
+# var room_size: Vector2i = Vector2i(288, 162)
+
+var updated_image: bool = false
 
 func _init() -> void:
 	Global.ground = self
 
-func _ready() -> void:
-	blood_image = Image.create(room_size.x, room_size.y, false, Image.FORMAT_RGBA8)
-	blood_image.fill(Color(0, 0, 0, 0))
+func _ready() -> void:	
+	blood_image = create_image(Global.room_size)
 	blood_texture.set_image(blood_image)
+	
+	resize_room(Global.room_size)
+	
+	Global.room_resized.connect(resize_room)
 	
 	#for i in range(50):
 	#	for j in range(50):
@@ -22,14 +27,42 @@ func _ready() -> void:
 	
 	# (material as ShaderMaterial).set_shader_parameter("blood", blood_texture)
 
+var filled_pixels: int = 0
+
+func _process(delta: float) -> void:
+	if updated_image:
+		blood_texture.update(blood_image)
+		updated_image = false
+
+func resize_room(new_size: Vector2i):
+	var new_image: Image = create_image(new_size)
+	
+	var x_offset: int = (new_image.get_width() - blood_image.get_width()) / 2
+	var y_offset: int = (new_image.get_height() - blood_image.get_height()) / 2
+	
+	for x in range(blood_image.get_width()):
+		for y in range(blood_image.get_height()):
+			new_image.set_pixel(x + x_offset, y + y_offset, blood_image.get_pixel(x, y))
+	
+	blood_texture.set_image(new_image)
+	blood_image = new_image
+	
+	texture.width = new_size.x
+	texture.height = new_size.y
+	
+	# blood_texture.draw_rect(blood_texture, Rect2(1, 1, 1, 1), false)
+
+func get_fill_ratio() -> float:
+	return filled_pixels / (float)(Global.room_size.x * Global.room_size.y)
+
 func paint_circle(circle_pos: Vector2i, radius: int) -> int:
 	radius += 2
-	circle_pos += room_size / 2
+	circle_pos += Global.room_size / 2
 	var painted_pixels = 0;
 	var start_x: int = max(circle_pos.x - radius - 10, 0)
-	var end_x: int = min(circle_pos.x + radius, room_size.x)
+	var end_x: int = min(circle_pos.x + radius, Global.room_size.x)
 	var start_y: int = max(circle_pos.y - radius - 10, 0)
-	var end_y: int = min(circle_pos.y + radius, room_size.y)
+	var end_y: int = min(circle_pos.y + radius, Global.room_size.y)
 	
 	for x in range(start_x, end_x):
 		for y in range(start_y, end_y):
@@ -58,11 +91,21 @@ func paint_pixel(x: int, y: int) -> bool:
 		return false
 	
 	blood_image.set_pixel(x, y, blood_color)
-	blood_texture.set_image(blood_image)
+	updated_image = true
 	return true
+
+func is_pixel_filled(x: int, y: int) -> bool:
+	if not is_in_image(x, y): return false
+	var current_color = blood_image.get_pixel(x, y)
+	return current_color.a != 0
 
 func is_in_image(x: int, y: int) -> bool:
 	if x < 0 or y < 0: return false
-	if x >= room_size.x or y >= room_size.y: return false
+	if x >= Global.room_size.x or y >= Global.room_size.y: return false
 	
 	return true
+
+func create_image(size: Vector2i) -> Image:
+	var img: Image = Image.create_empty(size.x, size.y, false, Image.FORMAT_RGBA8)
+	img.fill(Color(0, 0, 0, 0))
+	return img
--- scripts/global.gd
+++ scripts/global.gd
@@ -2,9 +2,17 @@ extends Node
 
 var blood: float = 100
 
+var room_size: Vector2i = Vector2i(288, 162)
+
 var player: Player
 var ground: Ground
+var main: Main
 
 ## signal enemy sends when getting eaten
 @warning_ignore("unused_signal")
 signal enemy_eaten(enemy: Enemy)
+
+
+## when room gets resized
+@warning_ignore("unused_signal")
+signal room_resized(size: Vector2i)
