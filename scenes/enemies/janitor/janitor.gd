98bbfc088efc3428467ffda314a37aae8d363a39
Sackboy495 <sackmike2007@icloud.com>
2026-02-28 22:54:05 -0700

    added janitor stuff

--- scenes/enemies/janitor/janitor.gd
+++ scenes/enemies/janitor/janitor.gd
@@ -14,4 +14,6 @@ func find_direction() -> void:
 	var enemy_position: Vector2 = position
 	var player_position: Vector2 = player.position
 	direction = enemy_position.direction_to(player_position)
-	
+
+func clean_blood() -> void:
+	pass
