d245fb9206eb680c6f8a12b639111b88e076d861
Mihai-Alexandru Radu <mira3628@colorado.edu>
2026-03-01 20:02:59 -0700

    fixed shield bug

--- scenes/enemies/shielded/shielded.gd
+++ scenes/enemies/shielded/shielded.gd
@@ -36,11 +36,9 @@ func find_direction() -> void:
 	var distance: float = enemy_position.distance_to(player_position)
 	# if player too close, go away from player otherwise go towards 
 	if abs(distance - distance_target) < DISTANCE_TOLERANCE:
-		direction = Vector2.ZERO
-	if distance < distance_target:
-		direction = -enemy_position.direction_to(player_position)
-	elif distance > distance_target:
 		direction = enemy_position.direction_to(player_position)
+	else:
+		direction = -enemy_position.direction_to(player_position)
 
 func change_distance():
 	distance_target = randf_range(distance_target_min, distance_target_max)
